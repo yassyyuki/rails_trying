369e43b5bf4c835de01b36f6b49d7eeaf5683a3f
yassyyuki <protutorial.05@gmail.com>
2019-10-03 19:04:53 +0900

    trying

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :shops
+
+  namespace :admin do
+    resources :lists, only: [:index, :show, :edit]
+  end
+
 end
